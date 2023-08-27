88c139ebe7ddbcf28587e1ab0ee6dbf3afa510e1
Guidotss <guidoolguin@hotmail.com>
2023-08-27 17:32:51 -0300

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'black': '#151619',
+        'dark_charcoal': '#1D1F22',
+        'slate_gray': '#2B2D31',
+        'steel_blue': '#35393F',
+        'graphite_gray': '#5A6069',
+        'dove_gray': '#7C8187',
+        'light_gray': '#C1C4CB',
+        'light_silver': '#E4E4E4',
+        'white_smoke': '#F5F5F5',
+        'white': '#FFFFFF',
+        'coral': '#E46643',
+        'apricot': '#F39765',
+      }
     },
   },
   plugins: [],
